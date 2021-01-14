53359ead072fffc9531905f49cc42344f07bfe23
Jaeseung Byun <dddwsd@github.com>
2021-01-14 21:46:11 +0900

    Environment setting from spring boot

---
+++ heretak/src/test/java/Heretak/heretak/HeretakApplicationTests.java
@@ -0,0 +1,13 @@
+package Heretak.heretak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeretakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
